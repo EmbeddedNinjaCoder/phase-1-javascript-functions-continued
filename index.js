391391f0676dd1a3c77f30c3b98707a983ef9f3b
German Santos <santos.german@gmail.com>
2023-04-17 20:33:15 -0700

    Functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  
+   let outputString = `This Saturday, I want to ${activity}!`
+  
+   return(outputString)
+}
+
+
+const mondayWork = function (activityMonday = "go to the office") {
+
+    let outputString = `This Monday, I will ${activityMonday}.`
+
+    return(outputString)
+  }
+
+  function wrapAdjective(highlight = "*") {
+    return function (param1 = "special") {
+        return `You are ${highlight}${param1}${highlight}!`
+        }
+    }
+   
+  
